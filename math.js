7564cda05470deef9486e856bdf5113d9a647b44
EvanBC1 <starfoxz13@gmail.com>
2019-05-20 16:17:56 -0700

    added recomendations

--- math.js
+++ math.js
@@ -7,7 +7,7 @@ var railCarbonPermile = .357148865;
 var subwayCarbonPerMile = .264555;
 var co2PerGallon = 19.6;
 var totalCarbon = 0;
-var mpg = 25;
+var carMpg = 25;
 var milesDriven = 10000;
 var percentMilesReplacedByBus = .5;
 var carbonReleasedByDriving = 0;
@@ -16,7 +16,7 @@ function calculateCarEmissions () {
   // Car emit: 19.6 pound per gallon of gas
   // Miles driven per year / MPG * 19.6
   // Example 10,000 / 30 * 19.6 = 6,533.33 lbs of CO2 per year
-  carbonReleasedByDriving = Math.round(milesDriven / mpg * co2PerGallon);
+  carbonReleasedByDriving = Math.round(milesDriven / carMpg * co2PerGallon);
   console.log(`${carbonReleasedByDriving} pounds of carbon produced by driving`);
   
 }
@@ -52,7 +52,7 @@ function calculateTravelEmissions () {
 }
 
 function recommendations (carbonReleasedByDriving) {
-  var busEfficiency = busCarbonPerMile / (co2PerGallon / mpg); 
+  var busEfficiency = busCarbonPerMile / (co2PerGallon / carMpg); 
   var carbonSaved = Math.round((carbonReleasedByDriving * percentMilesReplacedByBus) - ((carbonReleasedByDriving * percentMilesReplacedByBus) * busEfficiency)) ;
   console.log(`Driving a bus emmits only ${Math.round(busEfficiency * 100)}% of the carbon per mile that your car does. By bussing 50% of your miles driven than you could prevent ${carbonSaved} lbs of CO2 from being emmited anually.`); 
 }
