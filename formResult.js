da0c3f26bf8a954bdf75a74b205dc4133e457f37
paulacruz <cruz.amp12@gmail.com>
2019-05-20 14:50:51 -0700

    added bg -form itself

--- formResult.js
+++ formResult.js
@@ -4,27 +4,11 @@
 //calling form
 var infoForm = document.getElementById('informationForm')
 
-//global variable for results form, holds the instances of air, busway, railway, subway, 
-var travelSumArray = [];
+// creating table
+
+var summaryTable 
+
+//result chart
 
 
-
-//function to calculate travel summary
-function getRandomIntInclusive(travel, miles) {
-    travel = Math.ceil(travel);
-    miles = Math.floor(mile);
-    return Math.floor(Math.random() * (travel - min + 1)) + min;
-  }
-
-//constructor
-function Summary (air, bus, train, subway,) {
-    this.mpg = mpg;
-    this.milesDriven = milesDriven;
-    this.saco2PerGallon = co2PerGallon;
-    this.lbsOfCo2 = lbsOfCo2;
-  
-    //summary per array
-    
-  
-    //populating all array
     
