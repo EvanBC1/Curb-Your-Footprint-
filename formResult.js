669c756ef189b7fff4e8ae424132e70394e1bebc
paulacruz <cruz.amp12@gmail.com>
2019-05-21 11:54:21 -0700

    fixing event-listener

--- formResult.js
+++ formResult.js
@@ -2,73 +2,78 @@
 
 
 //calling form from HTML
-var infoForm = document.getElementById('infoForm');
+var submitInfo = document.getElementById('submitInfo');
 //calling table from HTML
 var tableResult = document.getElementById('formTable');
-var carbonList = document.getElementById('carbon-emission')
+//calling 
+var resultList = document.getElementById('carbon-emission');
 
-var testArrayForm = [];
+
 var totalCarbonForm = [];
-var newUser = [];
+var allData = [];
 
 
 
-//constructor 
+//constructor
 function User (air, bus, train, subway) {
-    this.airCarbonPermile = air;
-    this.busCarbonPermile = bus;
-    this.railCarbonPerMile = train;
-    this.subwayCarbonPerMile = subway;
-};
+  this.airCarbonPermile = air;
+  this.busCarbonPermile = bus;
+  this.railCarbonPerMile = train;
+  this.subwayCarbonPerMile = subway;
+}
 
 //stores test array of carbon emitted
 this.userInputArray= [];
-this.totalCarbonForm = [];
+
 
 //populatuing carbon array
-newUser.push(this)
+allData.push(this);
 
 
 
 //functions
 function renderHeaderRow(){
-    var trRl = document.createElement('tr');
-    var thEl = document.createElement('th');
-    thEl.textContent = 'air';
-    trEl.appenChild(thEl);
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = 'air';
+  trEl.appendChild(thEl);
 }
-//submit button 
+
+renderHeaderRow();
+
 
 //event handler
 function handleInformationForm (event){
-    event.preventDefault();
+  event.preventDefault();
 }
-var newAir = event.target.air.value;
-var newBus = event.target.busWay.value;
-var newTrain = event.target.railWay.value;
-var newSubway = even.target.subWay.value;
 
-new User (newAir, newBus, newTrain, newSubway);
+// var newAir = event.target.air.value;
+// var newBus = event.target.busWay.value;
+// var newTrain = event.target.railWay.value;
+// var newSubway = event.target.subWay.value;
 
-overAllTotal = 0;
 
-formTable.innerHTML = '';
+// new User (newAir, newBus, newTrain, newSubway);
+
+// overAllTotal = 0;
+
+// formTable.innerHTML = '';
 
 
 function renderList (){
-    for ( var i = 0; i < userInputArray.length; i++){
-        var liEl = document.createElement('li');
-        liEl.textContent = `${userInputArray[i].value} string here ${userInputArray[i].views} string here ${userInputArray[i].value} string here `;
-        carbonList.appendChild(liEl);
-    }
+  for ( var i = 0; i < userInputArray.length; i++){
+    var liEl = document.createElement('li');
+    liEl.textContent = `${userInputArray[i].value} string here ${userInputArray[i].views} string here ${userInputArray[i].value} string here `;
+    carbonList.appendChild(liEl);
+  }
 }
-    renderList();
+renderList();
 
 
 //event validator
-if(!event.target.says.value || !event.target.who.value){
-  alert('Fields cannot be empty!');
-};
+// if(!event.target.says.value || !event.target.who.value){
+//   alert('Fields cannot be empty!');
+// }
 
 //new instances 
 
@@ -76,7 +81,9 @@ if(!event.target.says.value || !event.target.who.value){
 
 
 //event listener
-submitInfo.addEventListener('click', handleSubmitClick);
+window.onload = function(){
+  submitInfo.addEventListener('click', handleSubmitClick);
+};
 function handleSubmitClick(){
   console.log('hello');
 }
