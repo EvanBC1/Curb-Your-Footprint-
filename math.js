b7d4a42023d1474a854b0a0506120cd8d15b1d1a
EvanBC1 <starfoxz13@gmail.com>
2019-05-21 17:02:01 -0700

    added caclulations

--- math.js
+++ math.js
@@ -11,6 +11,9 @@ var carMpg = 25;
 var milesDriven = 10000;
 var percentMilesReplacedByBus = .5;
 var carbonReleasedByDriving = 0;
+var carbonPerKWH = 1.04;
+var averageKWH = 11764;
+var solarSystem = 5;
 
 function calculateCarEmissions () {
   // Car emit: 19.6 pound per gallon of gas
@@ -18,6 +21,14 @@ function calculateCarEmissions () {
   // Example 10,000 / 30 * 19.6 = 6,533.33 lbs of CO2 per year
   carbonReleasedByDriving = Math.round(milesDriven / carMpg * co2PerGallon);
   console.log(`${carbonReleasedByDriving} pounds of carbon produced by driving`);
+}
+
+function calculateHousingEmissions () {
+  var CarbonReleasedFromElectricity = Math.round(averageKWH * carbonPerKWH);
+  console.log(`${CarbonReleasedFromElectricity} pounds of carbon released by your electricity usage`);
+  // KW of system * hours of full sun a day * days in a year
+  var solarProduction = solarSystem * 4 * 365;
+  console.log(`With solar you could be saving ${CarbonReleasedFromElectricity - solarProduction} pounds of carbon per year`);
 }
 
 function calculateTravelEmissions () {
@@ -66,7 +77,8 @@ function recommendations (carbonReleasedByDriving) {
   var carbonSaved = Math.round((carbonReleasedByDriving * percentMilesReplacedByBus) - ((carbonReleasedByDriving * percentMilesReplacedByBus) * busEfficiency)) ;
   console.log(`Driving a bus emmits only ${Math.round(busEfficiency * 100)}% of the carbon per mile that your car does. By bussing 50% of your miles driven than you could prevent ${carbonSaved} lbs of CO2 from being emmited anually.`);
   console.log(`Or if you rode a bike for those mile you would reduce your carbon emissions by ${carbonReleasedByDriving / 2} pounds per year`);
-  console.log(`By replacing old incandecent bulbs`); 
+  console.log(`For each incandecent bulb you replace you can reduce your carbon footprint by ${Math.round(carbonPerKWH * 37.2)} pounds a year`);
+  console.log(``) 
 }
 
 
@@ -84,5 +96,6 @@ if(localStorage.test) {
 }
 
 calculateCarEmissions();
+calculateHousingEmissions ();
 calculateTravelEmissions ();
 recommendations(carbonReleasedByDriving);
