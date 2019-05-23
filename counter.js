c8d678611a8fcb12c030070d03c11d0bffb34e1c
paulacruz <cruz.amp12@gmail.com>
2019-05-23 13:03:02 -0700

    added more css on index html

--- counter.js
+++ counter.js
@@ -6,7 +6,7 @@ var el = document.getElementById('counter');
 
 function incrementSeconds() {
   pounds += 2570000;
-  el.innerText = 'Humans have created ' + pounds + ' pounds of carbon since you have visited.';
+  el.innerText = 'You have created ' + pounds + ' pounds of carbon since you have visited.';
 }
 var cancel = setInterval(incrementSeconds, 500);
 incrementSeconds();
