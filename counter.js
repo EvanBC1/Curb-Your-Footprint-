2c220379c3b331db214912464a453ea8f18d3ba5
EvanBC1 <starfoxz13@gmail.com>
2019-05-23 16:02:44 -0700

    small edits

--- counter.js
+++ counter.js
@@ -6,7 +6,7 @@ var el = document.getElementById('counter');
 
 function incrementSeconds() {
   pounds += 2570000;
-  el.innerText = 'You have created ' + pounds + ' pounds of carbon since you have visited this site.';
+  el.innerText = 'Humans have released ' + pounds + ' pounds of carbon since you have visited this site.';
 }
 var cancel = setInterval(incrementSeconds, 500);
 incrementSeconds();
